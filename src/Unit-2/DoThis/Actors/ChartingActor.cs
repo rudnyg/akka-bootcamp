46b87aa502a7749c9b595f2c399413eebaee9f5a
Rudny Gacin <rudnyg@gmail.com>
2016-04-29 11:11:23 -0400

    Complete Unit 2, Lesson 2
    Changed ChartActor to ReceiveActor and added an AddSeries message for
    chart.

--- src/Unit-2/DoThis/Actors/ChartingActor.cs
+++ src/Unit-2/DoThis/Actors/ChartingActor.cs
@@ -6,7 +6,7 @@ using Akka.Actor;
 
 namespace ChartApp.Actors
 {
-    public class ChartingActor : UntypedActor
+    public class ChartingActor : ReceiveActor
     {
         #region Messages
 
@@ -20,6 +20,18 @@ namespace ChartApp.Actors
             public Dictionary<string, Series> InitialSeries { get; private set; }
         }
 
+        /// <summary>
+        /// Add a new <see cref="Series"/> to the chart
+        /// </summary>
+        public class AddSeries
+        {
+            public AddSeries(Series series)
+            {
+                Series = series;
+            }
+
+            public Series Series { get; private set; }
+        }
         #endregion
 
         private readonly Chart _chart;
@@ -33,16 +45,20 @@ namespace ChartApp.Actors
         {
             _chart = chart;
             _seriesIndex = seriesIndex;
+
+            Receive<InitializeChart>(ic => HandleInitialize(ic));
+            Receive<AddSeries>(addSeries => HandleAddSeries(addSeries));
         }
 
-        protected override void OnReceive(object message)
-        {
-            if (message is InitializeChart)
-            {
-                var ic = message as InitializeChart;
-                HandleInitialize(ic);
-            }
-        }
+        //Method no longer needed, Changed from UntypedActor to a ReceiveActor 
+        //protected override void OnReceive(object message)
+        //{
+        //    if (message is InitializeChart)
+        //    {
+        //        var ic = message as InitializeChart;
+        //        HandleInitialize(ic);
+        //    }
+        //}
 
         #region Individual Message Type Handlers
 
@@ -69,6 +85,15 @@ namespace ChartApp.Actors
             }
         }
 
+        private void HandleAddSeries(AddSeries series)
+        {
+            if (!string.IsNullOrEmpty(series.Series.Name) &&
+            !_seriesIndex.ContainsKey(series.Series.Name))
+            {
+                _seriesIndex.Add(series.Series.Name, series.Series);
+                _chart.Series.Add(series.Series);
+            }
+        }
         #endregion
     }
 }
